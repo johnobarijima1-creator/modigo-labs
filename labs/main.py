f8efa9d259424c5945d20c1c11b402f2a31ba631
johnobarijima1-creator <johnobarijima1@gmail.com>
2026-09-09 14:13:32 +0100

    Complete: Below is a function meant to count how many distinct visitor

--- labs/main.py
+++ labs/main.py
@@ -1,2 +1,5 @@
-def same_point(point1, point2):
-    return point1 == point2
+def count_unique_visitors(visitors):
+    unique_visitors = set(visitors)
+    return len(unique_visitors)
+
+print(count_unique_visitors(["ada", "bola", "ada"]))
