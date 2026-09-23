fbc469d883a79f7dfabba9c46aaaa064ea414668
johnobarijima1-creator <johnobarijima1@gmail.com>
2026-09-23 11:52:32 +0100

    Complete: Coordinate Distance Checker

--- challenges/main.py
+++ challenges/main.py
@@ -1,11 +1,4 @@
-def compare_hobbies(person1_hobbies, person2_hobbies):
-    hobbies1 = set(person1_hobbies)
-    hobbies2 = set(person2_hobbies)
-    return {
-        "shared": person1_hobbies & person2_hobbies,
-        "only_person1": person1_hobbies - person2_hobbies,
-        "only_person2": person2_hobbies - person1_hobbies,
-    }
+def same_point(point1, point2):
+    return point1 == point2
 
-
-print(compare_hobbies({"reading", "coding"}, {"coding", "gaming"}))
+print(same_point(1, 2), (1, 2))
